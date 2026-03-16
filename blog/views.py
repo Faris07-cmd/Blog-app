a020b22f87b8f0b38f024acc1ccbc45aebe7b728
Faris07-cmd <farisagunachew@icloud.com>
2026-03-16 13:23:29 +0300

    django blog app project with minimal user and register page (not linked yet)

---
+++ blog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+from .models import Post
+
+
+def home(request):
+    posts = Post.objects.all()
+
+    return render(request, "blog/home.html", {"posts": posts})
+
+
+def about(request):
+    return render(request, "blog/about.html")
+
+
+# Create your views here.
