39f59618dbca23c173df8f7addd30cafbe516a6e
Faris07-cmd <farisagunachew@icloud.com>
2026-04-10 02:02:24 +0300

    minor change to the save method in models

--- users/models.py
+++ users/models.py
@@ -5,16 +5,19 @@ from django.db import models
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    bio = models.CharField(max_length=200, blank=True)
+    bio = models.CharField(
+        max_length=200,
+    )
     image = models.ImageField(upload_to="profiles/", blank=True)
 
     def save(self, *args, **kwargs):
         try:
             old_profile = Profile.objects.get(user=self.user)
-            old_profile.image.delete(save=False)
+            if old_profile.image:
+                old_profile.image.delete()
+            super().save(*args, **kwargs)
         except Profile.DoesNotExist:
-            pass
-        super().save(*args, **kwargs)
+            super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.name}"
