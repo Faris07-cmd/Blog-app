e852fce2e826b230a0b1835bcc77e10f5cb2dad0
Faris07-cmd <farisagunachew@icloud.com>
2026-04-21 13:37:04 +0300

    minimal chnges

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -9,17 +10,22 @@ class Profile(models.Model):
         max_length=200,
     )
     image = models.ImageField(
-        upload_to="profiles/", default="profiles/default.jpg.webp", blank=True
+        upload_to="profiles/", default="profiles/images.png", blank=True
     )
 
     def save(self, *args, **kwargs):
         try:
             old_profile = Profile.objects.get(user=self.user)
-            if old_profile.image:
+            if old_profile.image and old_profile.image.name != "images.png":
                 old_profile.image.delete(save=False)
-            super().save(*args, **kwargs)
         except Profile.DoesNotExist:
-            super().save(*args, **kwargs)
+            pass
+        super().save(*args, **kwargs)
+        if self.image:
+            img = Image.open(self.image.path)
+            if img.height > 300 or img.width > 300:
+                img.thumbnail((300, 300))
+                img.save(self.image.path)
 
     def __str__(self):
         return f"{self.name}"
