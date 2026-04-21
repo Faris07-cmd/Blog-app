8564a0ba2d8ccdf1fce48ada55278b82790b786e
Faris07-cmd <farisagunachew@icloud.com>
2026-04-21 13:36:06 +0300

    used a class instead of function

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,9 @@
 from django.contrib import messages
 from django.shortcuts import redirect, render
+from django.views.generic import ListView
 
 from .forms import PostForm
 from .models import Post
-
-
-def home(request):
-    posts = Post.objects.all()
-
-    return render(request, "blog/home.html", {"posts": posts})
 
 
 def about(request):
@@ -17,9 +12,17 @@ def about(request):
 
 def post_form(request):
     form = PostForm(request.POST or None)
-    if required.method == "POST" and form.is_valid():
-        Post.author = request.user
+    if request.method == "POST" and form.is_valid():
+        Post.auhtor = request.user
         form.save()
-        message.success(request, "Posted")
+
+        messages.success(request, "Posted")
         return redirect("blog-home")
     return render(request, "blog/post.html", {"form": form})
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/home.html"
+    context_object_name = "posts"
+    ordering = ["-date_posted"]
